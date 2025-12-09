60622bf63ccc21c33fa32e9f952be1331cd0dbce
Zoltan Dienes <dragonka79@gmail.com>
2025-12-09 15:26:05 +0100

    cosmetic fix

--- NX-OS/NX-API REST/create_if_descr.py
+++ NX-OS/NX-API REST/create_if_descr.py
@@ -1,13 +1,12 @@
 import json
 from pprint import pprint
 import requests
-from device_info import device
 from get_token import cookies, HOST, VERIFY, TIMEOUT
 
 requests.packages.urllib3.disable_warnings()  # keep for lab; prefer proper CA bundle in production
 
 intf = "eth1/2"
-descr = "SampleString_1234567"
+descr = "SampleString_12345678"
 
 URL = f"https://{HOST}/api/mo/sys/intf/phys-[{intf}].json"
 method = "POST"
